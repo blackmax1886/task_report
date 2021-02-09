f8d6d5ebdad2e18c8aad855717a31f5a6d1f02cf
yorukuma <blackmax1886@gmail.com>
2021-02-09 23:51:02 +0900

    create Task & Subtask model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=50)
+    deadline = models.DateTimeField()
+    # pro_rate = models.IntegerField(verbose_name='progress rate')
+
+    def __str__(self):
+        return self.task_name
+
+
+class Subtask(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    subtask_name = models.CharField(max_length=50)
+    deadline = models.DateTimeField()
