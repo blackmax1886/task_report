a7487bf235f0f1d8d60bdb71a57d9f5414700371
yorukuma <blackmax1886@gmail.com>
2021-02-10 09:20:46 +0900

    add __str__ to Subtask class

--- tasks/models.py
+++ tasks/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Task(models.Model):
     task_name = models.CharField(max_length=50)
     deadline = models.DateTimeField()
+
     # pro_rate = models.IntegerField(verbose_name='progress rate')
 
     def __str__(self):
@@ -14,3 +15,6 @@ class Subtask(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     subtask_name = models.CharField(max_length=50)
     deadline = models.DateTimeField()
+
+    def __str__(self):
+        return self.subtask_name
