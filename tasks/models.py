4967bf8f5f1ed2fe635ec44d1dad09991b401f8c
yorukuma <blackmax1886@gmail.com>
2021-02-11 17:17:43 +0900

    add progress rate method to Task model

--- tasks/models.py
+++ tasks/models.py
@@ -4,17 +4,23 @@ from django.db import models
 class Task(models.Model):
     task_name = models.CharField(max_length=50)
     deadline = models.DateTimeField()
-
-    # pro_rate = models.IntegerField(verbose_name='progress rate')
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.task_name
+
+    def progress_rate(self):
+        from math import floor
+        subtasks_num = self.subtask_set.count()
+        complete_num = self.subtask_set.filter(status=True).count()
+        return floor((complete_num / subtasks_num) * 100)
 
 
 class Subtask(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     subtask_name = models.CharField(max_length=50)
     deadline = models.DateTimeField()
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.subtask_name
