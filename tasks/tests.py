106b73d2f2d0250be24044b9bda8adbb83e71e2a
yorukuma <blackmax1886@gmail.com>
2021-02-11 18:22:32 +0900

    add progress rate method test for tasks with no subtask

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Task
+
+
+class TaskModelTests(TestCase):
+    def test_progress_rate_with_no_subtask(self):
+        """
+        progress_rate() returns None for tasks that have no subtask
+        """
+        nosub_task = Task()
+        self.assertIs(nosub_task.progress_rate(), None)
