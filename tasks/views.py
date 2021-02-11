017152840122d65ddbf1a8639bae6bd4216c91ee
yorukuma <blackmax1886@gmail.com>
2021-02-11 19:29:58 +0900

    change order of task list on index page & list name (closest_task_list)

--- tasks/views.py
+++ tasks/views.py
@@ -3,17 +3,16 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.views.generic import TemplateView
 
-
 from .models import Task, Subtask
 
 
 class IndexView(generic.ListView):
     template_name = 'tasks/index.html'
-    context_object_name = 'latest_task_list'
+    context_object_name = 'closest_task_list'
 
     def get_queryset(self):
-        """Return the 5 closest tasks to deadline."""
-        return Task.objects.order_by('-deadline')[:5]
+        """Return the 5 closest uncompleted tasks to deadline."""
+        return Task.objects.filter(status=False).order_by('-deadline')[:5:-1]
 
 
 class DetailView(generic.DetailView):
