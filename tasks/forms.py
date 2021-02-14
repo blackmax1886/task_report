b39b75477218e471c944d45903d755790a2adad9
yorukuma <blackmax1886@gmail.com>
2021-02-14 17:07:10 +0900

    apply bootstrap to create view and add calender

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,12 +1,21 @@
 from django import forms
+import bootstrap_datepicker_plus as datetimepicker
 
 from .models import Task
 
 
 class TaskForm(forms.ModelForm):
+    # deadline = forms.SplitDateTimeField(label='期限(日時)')
+
     class Meta:
         model = Task
-        fields = ("task_name", "deadline")
+        fields = ("task_name", 'deadline')
         widgets = {
-            'deadline': forms.SelectDateWidget
+            'deadline': datetimepicker.DatePickerInput(
+                format='%Y-%m-%d',
+                options={
+                    'locale': 'ja',
+                    'dayViewHeaderFormat': 'YYYY年 MMMM',
+                }
+            )
         }
