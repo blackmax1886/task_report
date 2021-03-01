6ab9ff6904b0ad66bb94a8baafc96a14edf2a804
yorukuma <blackmax1886@gmail.com>
2021-03-01 16:40:37 +0900

    add signup page and view

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,20 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from users.models import User
 
 
 class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import (LoginView, LogoutView)
+from django.urls import reverse_lazy
 
 from django.views import generic
-from .forms import LoginForm
+from .forms import LoginForm, SignupForm
 
 
 class Top(generic.TemplateView):
@@ -17,3 +20,15 @@ class Login(LoginView):
 
 class Logout(LoginRequiredMixin, LogoutView):
     template_name = 'users/login.html'
+
+
+class Signup(generic.CreateView):
+    form_class = SignupForm
+    template_name = "users/signup.html"
+    success_url = reverse_lazy('users:top')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.object = user
+        return HttpResponseRedirect(self.get_success_url())
